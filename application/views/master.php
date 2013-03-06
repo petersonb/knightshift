7dd9fc8cb54891961441bd54ba156aed1b4b2358
Brett Peterson <bepeterson14@gmail.com>
2013-03-06 06:04:27 -0600

    added style to the site

--- application/views/master.php
+++ application/views/master.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<link href="<?php echo base_url("css/style.css"); ?>" type="text/css" rel="stylesheet" />
 <style type="text/css" >
 <?php if ($this->session->userdata('employee_id')): ?>
 body {
@@ -19,25 +20,27 @@ body {
 <title><?php if (isset($title)) echo $title; ?></title>
 </head>
 <body>
-  <a href="<?php echo base_url('main/logout') ?>">Logout</a>
-  <?php if ($this->session->userdata('department_context')): ?>
-  <a href="<?php echo base_url('departments/unset_context'); ?>">Unset Context</a>
-  <?php endif; ?>
-  <?php
-    if (isset($content))
-      {
-	if (is_array($content))
-	  {
-	    foreach ($content as $c_view)
-	      {
-		$this->load->view('content/'.$c_view);
-	      }
-	  }
-	else
-	  {
-	    $this->load->view('content/'.$content);
-	  }
-      }
-  ?>
+  <div id="container">
+    <a href="<?php echo base_url('main/logout') ?>">Logout</a>
+    <?php if ($this->session->userdata('department_context')): ?>
+    <a href="<?php echo base_url('departments/unset_context'); ?>">Unset Context</a>
+    <?php endif; ?>
+    <?php
+      if (isset($content))
+	{
+	  if (is_array($content))
+	    {
+	      foreach ($content as $c_view)
+		{
+		  $this->load->view('content/'.$c_view);
+		}
+	    }
+	  else
+	    {
+	      $this->load->view('content/'.$content);
+	    }
+	}
+    ?>
+  </div>
 </body>
 </html>
