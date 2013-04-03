7690c29a15c5f341c8297d9b86c2b4b40aeda24b
Brett Peterson <bepeterson@petersonb.com>
2013-04-03 18:10:00 -0500

    some documenting for hours controller

--- application/controllers/hours.php
+++ application/controllers/hours.php
@@ -1,149 +1,205 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Hours Controller
+ *
+ * @author Brett Peterson
+ *
+ * Handles everything to do with the hours
+*/
 class Hours extends CI_Controller {
 
-  public function index()
-  {
-    $data['title'] = 'Hours';
-    $this->load->view('master',$data);
-  }
+	public function __construct() {
+		parent::__construct();
+		$this->admin_id = $this->session->userdata('admin_id');
+		$this->employee_id = $this->session->userdata('employee_id');
+		$this->department_id = $this->session->userdata('department_id');
+		$this->department_context = $this->session->userdata('department_context');
+	}
 
-  public function edit_time($id = NULL)
-  {
-    if (!$id and !$this->input->post())
-      redirect('main');
-    else
-      {
-	$this->load->helper('form');
-	$this->load->library('form_validation');
-	
-	$this->form_validation->set_rules('date','Date','required');
-	$this->form_validation->set_rules('time_in','Time In', 'required');
-	$this->form_validation->set_rules('time_out','Time Out','required');
-	
-	if ($this->form_validation->run())
-	  {
-	    $h = new Hour($id);
-	    $h->date = $this->input->post('date');
-	    $h->time_in = $this->input->post('time_in');
-	    $h->time_out = $this->input->post('time_out');
-	    $h->save();
-	    redirect('hours/view_all');
-	  }
-	$h = new Hour($id);
-	$h->department->get();
-	
-	$data['hour'] = $h;
-	$data['title'] = 'Edit Time';
-	$data['content'] = 'hours/edit_time';
-	$this->load->view('master',$data);
-      }
-  }
+	/**
+	 * Hours
+	 *
+	 * Doesn't do anything yet :D
+	 */
+	public function index()
+	{
+		$data['title'] = 'Hours';
+		$this->load->view('master',$data);
+	}
 
-  public function log_time()
-  {
-    $this->load->helper(array('form','date'));
-    $this->load->library('form_validation');
+	/**
+	 * Edit Time
+	 *
+	 * Allows editing of a time that has already been logged.
+	 *
+	 * @param string $id
+	 * 	- Hour ID
+	 */
+	public function edit_time($id = NULL)
+	{
+		// TODO NEEDS SECURITY
+		if (!$id and !$this->input->post())
+		{
+			redirect('main');
+		}
 
-    $did = $this->session->userdata('department_id');
-    $dct = $this->session->userdata('department_context');
-    if ($did)
-      $d = new Department($did);
-    elseif($dct)
-      $d = new Department($dct);
+		$this->load->helper('form');
+		$this->load->library('form_validation');
 
-    $data['department']['name'] = $d->name;
-    
-    $eid = $this->session->userdata('employee_id');
-    if (!$eid)
-      $data['no_eid'] = TRUE;
-    else
-      $data['no_eid'] = FALSE;
+		// TODO MORE RULES
+		$this->form_validation->set_rules('date','Date','required');
+		$this->form_validation->set_rules('time_in','Time In', 'required');
+		$this->form_validation->set_rules('time_out','Time Out','required');
 
-    $this->form_validation->set_rules('date','Date','requ red');
-    $this->form_validation->set_rules('time_in','Time-in','required');
-    $this->form_validation->set_rules('time_out','Time-out','required');
-    if ($this->form_validation->run())
-      {
-	$h = new Hour();
+		if ($this->form_validation->run())
+		{
+			$h = new Hour($id);
+			$h->date = $this->input->post('date');
+			$h->time_in = $this->input->post('time_in');
+			$h->time_out = $this->input->post('time_out');
+			$h->save();
+			redirect('hours/view_all');
+		}
+		$h = new Hour($id);
+		$h->department->get();
 
-	if (!$eid)
-	  $e = new Employee($this->input->post('employee_id'));
-	else
-	  $e = new Employee($eid);
+		$data['hour'] = $h;
+		$data['title'] = 'Edit Time';
+		$data['content'] = 'hours/edit_time';
+		$this->load->view('master',$data);
+	}
 
-	$h->date = date_std_mysql($this->input->post('date'));
-	$h->time_in = $this->input->post('time_in');
-	$h->time_out = $this->input->post('time_out');
-	$h->save($e);
-	$h->save($d);
-      }
+	/**
+	 * Log Time
+	 *
+	 * Allows employees or departments to log time to the database
+	 * for their departments.
+	 */
+	public function log_time()
+	{
+		$this->load->helper(array('form','date'));
+		$this->load->library('form_validation');
 
-    $data['title'] = 'Log Time';
-    $data['css'] = 'calendar_widget/jquery-ui';
-    $data['javascript'] = array('jquery','jquery-ui','hours/date');
-    $data['content'] = 'hours/log_time';
-    $this->load->view('master',$data);
-  }
+		// If logging hour as department
+		if ($this->department_id)
+		{
+			$d = new Department($this->department_id);
+		}
 
-  public function view_all()
-  {
-    $eid = $this->session->userdata('employee_id');
-    $aid = $this->session->userdata('admin_id');
-    $dct = $this->session->userdata('department_context');
-    $did = $this->session->userdata('department_id');
-    if ($dct)
-      $d = new Department($dct);
-    elseif ($did)
-      $d = new Department($did);
-    elseif ($eid)
-      {
-	$e = new Employee($eid);
-	$data['employee'] = array('id'=>$e->id);
-	$d = $e->department->get();
-      }
-    elseif ($aid)
-      {
-	$a = new Admin($aid);
-	$d = $a->department->get();
-      }
-    
-    foreach ($d as $dept)
-      {
-	if ($eid)
-	  $dept->hour->where('employee_id',$eid);
-	$data['department'][$dept->id] = array('name'=>$dept->name,
-					       'hours'=>$dept->hour->get());
-      }
+		// If not a department, must have department context
+		elseif($this->department_context)
+		{
+			$d = new Department($this->department_context);
+		}
 
-    $data['title'] = 'View All Hours';
-    $data['css'] = array('slickgrid/slick.grid',
-			 'slickgrid/css/smoothness/jquery-ui-1.8.16.custom'
-			 );
+		$data['department']['name'] = $d->name;
 
-    $data['content'] = 'hours/view_all';
-    $this->load->view('master',$data);
-  }
+		if (!$this->employee_id)
+		{
+			$data['no_eid'] = TRUE;
+		}
+		else
+		{
+			$data['no_eid'] = FALSE;
+		}
 
-  public function employee_hours($id)
-  {
-    $this->load->helper('date');
-    $e = new Employee($id);
-    $e->hour->get();
-    
-    $data = array();
-    foreach ($e->hour as $h)
-      {
-	array_push($data,array('id'=>$h->id,
-			       'date'=>date_mysql_std($h->date),
-			       'time_in'=>$h->time_in,
-			       'time_out'=>$h->time_out));
-	
-      }
-    
-    echo json_encode($data);
-  }
-  
+		// TODO MORE RULES
+		$this->form_validation->set_rules('date','Date','required');
+		$this->form_validation->set_rules('time_in','Time-in','required');
+		$this->form_validation->set_rules('time_out','Time-out','required');
+
+		if ($this->form_validation->run())
+		{
+			$h = new Hour();
+
+			if (!$this->employee_id)
+			{
+				$e = new Employee($this->input->post('employee_id'));
+			}
+			else
+			{
+				$e = new Employee($this->employee_id);
+			}
+
+			$h->date = date_std_mysql($this->input->post('date'));
+			$h->time_in = $this->input->post('time_in');
+			$h->time_out = $this->input->post('time_out');
+			$h->save($e);
+			$h->save($d);
+		}
+
+		$data['title'] = 'Log Time';
+		$data['css'] = 'calendar_widget/jquery-ui';
+		$data['javascript'] = array('jquery','jquery-ui','hours/date');
+		$data['content'] = 'hours/log_time';
+		$this->load->view('master',$data);
+	}
+
+
+	/**
+	 * View All Hours
+	 *
+	 * Allows All hours to be viewed. Varies depending on type of access.
+	 */
+	public function view_all()
+	{
+		if ($this->department_context)
+		{
+			$d = new Department($this->department_context);
+		}
+		elseif ($this->department_id)
+		{
+			$d = new Department($this->department_id);
+		}
+		elseif ($this->employee_id)
+		{
+			$e = new Employee($this->employee_id);
+			$data['employee'] = array('id'=>$e->id);
+			$d = $e->department->get();
+		}
+		elseif ($this->admin_id)
+		{
+			$a = new Admin($this->admin_id);
+			$d = $a->department->get();
+		}
+
+		foreach ($d as $dept)
+		{
+			if ($this->employee_id)
+			{
+				$dept->hour->where('employee_id',$this->employee_id);
+			}
+			$data['department'][$dept->id] = array(
+					'name'=>$dept->name,
+					'hours'=>$dept->hour->get()
+			);
+		}
+
+		$data['title'] = 'View All Hours';
+		$data['content'] = 'hours/view_all';
+		$this->load->view('master',$data);
+	}
+
+	public function employee_hours($id)
+	{
+		$this->load->helper('date');
+		$e = new Employee($id);
+		$e->hour->get();
+
+		$data = array();
+		foreach ($e->hour as $h)
+		{
+			array_push($data,array('id'=>$h->id,
+			'date'=>date_mysql_std($h->date),
+			'time_in'=>$h->time_in,
+			'time_out'=>$h->time_out));
+
+		}
+
+		echo json_encode($data);
+	}
+
 }
 
 /* End of file hours.php */
