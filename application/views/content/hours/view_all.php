9509acd54cd3d19c1ca92582d43e670606bde634
Brett Peterson <bepeterson@petersonb.com>
2013-04-05 08:26:42 -0500

    all hours tables now sort by date

--- application/views/content/hours/view_all.php
+++ application/views/content/hours/view_all.php
@@ -1,15 +1,19 @@
 <input type="hidden" id="x" value="1" />
 <h1>View All Hours</h1>
-
+<?php $sortCol=0; ?>
 <table class="dataTable" id="dataTable">
 	<thead>
 		<tr>
 			<?php if (!$this->department_context && !$this->department_id):?>
+			<?php $sortCol++; ?>
 			<th>Department</th>
 			<?php endif; ?>
+			
 			<?php if (!$this->employee_id): ?>
+			<?php $sortCol++; ?>
 			<th>Employee</th>
 			<?php endif; ?>
+			
 			<th>Date</th>
 			<th>Time In</th>
 			<th>Time Out</th>
@@ -18,3 +22,4 @@
 	<tbody>
 	</tbody>
 </table>
+<input type="hidden" value="<?php echo $sortCol; ?>" id="sortCol"/>
--- javascript/hours/dept_hours.js
+++ javascript/hours/dept_hours.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-
 	$('#dataTable').dataTable({
 		"bProcessing" : true,
 		"sAjaxSource" : "/hours/department_hours",
-		"aaSorting" :  [[1,"desc"]]
+		"aaSorting" :  [[$("#sortCol").val(),"desc"]]
 	});
 });
