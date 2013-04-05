af6985117c4cbad9d4423668d8f80bef092a7c0c
Brett Peterson <bepeterson14@gmail.com>
2013-04-05 01:15:35 -0500

    updated format of create_department

--- application/views/forms/departments/create.php
+++ application/views/forms/departments/create.php
@@ -1,13 +1,42 @@
+<?php 
+$inputs = array(
+		array(
+				'label'=>'Department Name',
+				'name'=>'name',
+				'value'=>set_value('name')
+		),
+		array(
+				'label'=>'Login Name',
+				'name'=>'login_name',
+				'value'=>set_value('login_name')
+		),
+		array(
+				'label'=>'Department Id',
+				'name'=>'id',
+				'value'=>set_value('id')
+		)
+);
+
+$passwords = array (
+		array(
+				'label'=>'Password',
+				'name'=>'password'
+		),
+		array(
+				'label'=>'Confirm Password',
+				'name'=>'confirm'
+		)
+);
+?>
+
+<?php echo validation_errors(); ?>
 <?php echo form_open('departments/create'); ?>
-Department Name: <input type="text" name="name" />
-<br />
-Login Name: <input type="text" name="login_name" />
-<br />
-Department ID: <input type="text" name="id" />
-<br />
-Password: <input type="password" name="password" />
-<br />
-Confirm Password: <input type="password" name="confirm" />
-<br />
-<input type="submit" value="Create Department" />
+<table>
+	<?php form_input_bank($inputs); ?>
+	<?php form_password_bank($passwords); ?>
+	<tr>
+		<td><?php echo form_submit('submit','Create Department'); ?>
+		</td>
+	</tr>
+</table>
 </form>
