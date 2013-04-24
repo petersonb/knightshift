712d4e64a2c0f2d9101a50a87652af735744ea67
Brett Peterson <bepeterson@petersonb.com>
2013-04-23 23:11:27 -0500

    got stupid date shit figured out

--- application/views/forms/excel/select.php
+++ application/views/forms/excel/select.php
@@ -1,11 +1,19 @@
 <?php
-foreach(range(1,12) as $mval)
+for ($i = 1; $i < 13; $i++)
 {
-	echo date('F',intval($mval));
-	$months[$mval] = date('F',5);
-} 
-var_dump($months);
+	$dval = mktime('0','0','0',strval($i),date('d'),date('Y'));
+	$months[$i] = array('val'=>date('m',$dval),'name'=>date('F',$dval));
+}
 ?>
 
-<?php form_open('excel/admin'); ?>
+<?php form_open('excel/admin_generated'); ?>
+<select>
+	<?php foreach ($months as $m): ?>
+	<option value="<?php echo $m['val']; ?>">
+		<?php echo $m['name']; ?>
+	</option>
+	<?php endforeach; ?>
+</select>
+<br />
+<input type="submit" value="submit" />
 </form>
