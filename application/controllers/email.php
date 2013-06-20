a132739b841fc1d589198b99aa811d86cf44db3f
Brett Peterson <bepeterson@petersonb.com>
2013-06-20 17:52:14 -0500

    starting an admin_department_to_employees method for email controller

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -13,6 +13,8 @@ class Email extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		
+		$this->load->library('email');
 
 		$this->employee_id = $this->session->userdata('employee_id');
 		$this->admin_id = $this->session->userdata('admin_id');
@@ -21,14 +23,21 @@ class Email extends CI_Controller {
 
 	public function index ()
 	{
-		$this->load->library('email');
-
 		$this->email->from('brett@petersonb.com', 'Brett Peterson');
 		$this->email->to('bepeterson14@gmail.com');
 		$this->email->subject('Test');
 		$this->email->message('<h1>Super test!</h1>');
 		$this->email->send();
 		echo $this->email->print_debugger();
+	}
+
+	private function admin_department_to_employees($department_id, $employee_ids, $subject,$message)
+	{
+		$admin = new Admin($this->admin_id);
+		
+		$this->email->from($admin->email, "{$admin->firstname} {$admin->lastname}");
+		$this->email->subject($subject);
+		$this->email->message($message);
 	}
 }
 
