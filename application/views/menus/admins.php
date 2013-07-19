8fd60a6116fa93b60e352ef7cb0bcc9e86d5784d
Brett Peterson <bepeterson@petersonb.com>
2013-07-18 20:49:08 -0500

    style logic done for admins

--- application/views/menus/admins.php
+++ application/views/menus/admins.php
@@ -33,38 +33,94 @@ else
 $menu_items = array(
 		array('base'=>'admins',
 				'name'=>'Home',
+				'context'=>'no',
 				'dropdown_items' => array(),
+		),
+		array('base'=>'employees',
+				'name'=>'Employees',
+				'context'=>'yes',
+				'dropdown_items' => array(
+						array('base'=>'employees/view_all',
+								'name'=>'View All',
+								'context'=>'yes'
+						),
+				),
+		),
+		array('base'=>'hours',
+				'name'=>'Hours',
+				'context'=>'yes',
+				'dropdown_items' => array(
+						array('base'=>'hours/log_time',
+								'name'=>'Log Time',
+								'context'=>'yes'
+						),
+						array('base'=>'hours/view_all',
+								'name'=>'View All',
+								'context'=>'yes'
+						),
+				)
+		),
+		array('base'=>'departmens',
+				'name'=>'Departments',
+				'context' => 'no',
+				'dropdown_items' => array(
+						array('base'=>'departments/create',
+								'name'=>'Create',
+								'context'=>'no'
+						),
+						array('base'=>'departments/add_employee',
+								'name'=>'Add Employee',
+								'context'=>'yes'
+						)
+				)
+		),
+		array('base'=>'excel',
+				'name'=>'Excel',
+				'context'=>'yes',
+				'dropdown_items' => array()
 		),
 		array('base'=>'admins',
 				'name'=>'Profile',
+				'context'=>'no',
 				'dropdown_items' => array(
 
 						array('base'=>'admins/edit_profile',
 								'name'=>'Edit Profile',
-								'dropdown_items' => array()
+								'context'=>'no',
 						),
 						array('base'=>'admins/change_password',
 								'name'=>'Change Password',
-								'dropdown_items' => array()
+								'context'=>'no',
+						),
+						array('base'=>'main/logout',
+								'name'=>'Logout',
+								'context'=>'no',
 						)
 				)
-		)
+		),
+		array('base'=>'departments/unset_context',
+				'name'=>'Unset Context',
+				'context'=>'yes',
+				'dropdown_items' => array()
+		),
 );
 ?>
 
-<ul class="dropdown">
+<ul>
 	<?php foreach ($menu_items as $top_item): ?>
+	<?php if ( $top_item['context'] == 'no' || ($top_item['context'] == 'yes' && $context)): ?>
 	<li><a href="<?php echo base_url($top_item['base']); ?>"><?php echo $top_item['name']; ?>
-	<?php if (count($top_item['dropdown_items']) > 0): ?>
-	<ul>
-	</a> 
-	<?php foreach ($top_item['dropdown_items'] as $dropdown_item): ?>
+	</a> <?php if (count($top_item['dropdown_items']) > 0): ?>
+		<ul>
+			<?php foreach ($top_item['dropdown_items'] as $dropdown_item): ?>
+			<?php if ( $dropdown_item['context'] == 'no' || ($dropdown_item['context'] == 'yes' && $context)): ?>
 			<li><a href="<?php echo base_url($dropdown_item['base']); ?>"><?php echo $dropdown_item['name']; ?>
 			</a></li>
-	<?php endforeach; ?>
-	</ul>
-	<?php endif; ?>
+			<?php endif; ?>
+			<?php endforeach; ?>
+		</ul> <?php endif; ?>
 	</li>
+	<?php endif;?>
 	<?php endforeach; ?>
 </ul>
 <div style="clear: both;"></div>
