0e61dfb7c2baf2aec424c9a2bcfcfe86086367dd
Brett Peterson <bepeterson14@gmail.com>
2013-07-27 14:46:14 -0500

    updated email type

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -108,11 +108,11 @@ class Main extends CI_Controller {
 			{
 				$message = "<h3>Dear {$user->firstname},</h3>";
 				$message .= "<p>You have requested to have your password reset. Please click the following link.</p>";
-				$message .= "<a href=\"".base_url('main/reset_password')."?pwrc=".$code."\">Click here</a>";
+				$message .= "<a href=\"".base_url('main/password_reset')."?pwrc=".$code."\">Click here</a>";
 				$message .= "<p>If you did not request a password change, please contact <a href=\"support@knightshift-track.com\">KnightShift Support</a>.</p>";
 				$message .= "<br /><p>Thanks,<br />KnightShift Support</p>";
 				$this->email->from('support@knightshift-track.com', 'KnightShift Master');
-				$this->mailtype="html";
+				$this->email="html";
 				$this->email->to($email);
 				$this->email->subject('Password Reset');
 				$this->email->message($message);
