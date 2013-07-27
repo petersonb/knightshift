5ab4d2a40e25025c3d22e0ebe83c1c91566f6af0
Brett Peterson <bepeterson14@gmail.com>
2013-07-27 16:20:32 -0500

    new layout for log time form

--- application/views/forms/hours/log_time.php
+++ application/views/forms/hours/log_time.php
@@ -12,96 +12,108 @@ else
 
 
 <table>
-	<?php if (isset($no_eid) && $no_eid): ?>
 	<tr>
-		<td>Employee:</td>
-		<td><select name="employee_id">
-				<?php foreach ($employees as $emp): ?>
-				<option value="<?php echo $emp['id'] ; ?>">
-					<?php echo $emp['firstname']. ' ' . $emp['lastname']; ?>
-				</option>
-				<?php endforeach;?>
-		</select>
-		</td>
-	</tr>
-	<?php endif; ?>
-	<tr>
-		<td>Date:</td>
-		<td><input id="datepicker" type="text" value="<?php echo $date; ?>"
-			name="date" /></td>
-		<br />
-	</tr>
-	<tr>
-		<td>Time-in :</td>
+		<?php if (isset($no_eid) && $no_eid): ?>
 		<td>
 			<table>
 				<tr>
-					<td><select name="hour_in">
-							<?php for ($i = 1; $i < 13; $i++):?>
-							<?php if ($i < 10) $i = "0".$i; ?>
-							<option value="<?php echo $i; ?>"
-							<?php if ($time_in['hour'] == $i) echo 'selected = "selected"'; ?>>
-								<?php echo $i; ?>
+					<td style="float:left;">Employee:</td>
+					<td><select name="employee_id" size="6">
+							<?php foreach ($employees as $emp): ?>
+							<option value="<?php echo $emp['id'] ; ?>">
+								<?php echo $emp['firstname']. ' ' . $emp['lastname']; ?>
 							</option>
-							<?php endfor; ?>
-					</select></td>
-					<td>:</td>
-					<td><select name="minute_in">
-							<?php for ($i = 0; $i < 60; $i+=15):?>
-							<?php if ($i == 0) $i = "00"; ?>
-							<option value="<?php echo $i; ?>"
-							<?php if ($time_in['minute'] == $i) echo 'selected="selected"'; ?>>
-								<?php echo $i; ?>
-							</option>
-							<?php endfor; ?>
-					</select></td>
-					<td><select name="pm_in"><option value="am"
-					<?php if ($time_in['period'] == "am") echo 'selected="selected"'; ?>>AM</option>
-							<option value="pm"
-							<?php if ($time_in['period'] == "pm") echo 'selected="selected"'; ?>>PM</option>
+							<?php endforeach;?>
 					</select>
 					</td>
 				</tr>
 			</table>
 		</td>
-	</tr>
-	<tr>
-		<td>Time-out :</td>
+		<?php endif; ?>
+
 		<td>
-			<table>
+			<table style="float:right">
 				<tr>
-					<td><select name="hour_out">
-							<?php for ($i = 1; $i < 13; $i++):?>
-							<?php if ($i < 10) $i = "0".$i; ?>
-							<option value="<?php echo $i; ?>"
-							<?php if ($time_out['hour'] == $i) echo 'selected = "selected"'; ?>>
-								<?php echo $i; ?>
-							</option>
-							<?php endfor; ?>
-					</select></td>
-					<td>:</td>
-					<td><select name="minute_out">
-							<?php for ($i = 0; $i < 60; $i+=15): ?>
-							<?php if ($i == 0) $i = "00"; ?>
-							<option value="<?php echo $i; ?>"
-							<?php if ($time_out['minute'] == $i) echo 'selected = "selected"'; ?>>
-								<?php echo $i; ?>
-							</option>
-							<?php endfor; ?>
-					</select></td>
-					<td><select name="pm_out"><option value="am"
-					<?php if ($time_out['period'] == "am") echo 'selected = "selected"'; ?>>AM</option>
-							<option value="pm"
-							<?php if ($time_out['period'] == 'pm') echo 'selected = "selected"'; ?>>PM</option>
-					</select>
+					<td>Date:</td>
+					<td><input id="datepicker" type="text" value="<?php echo $date; ?>"
+						name="date" /></td>
+					<br />
+				</tr>
+				<tr>
+					<td>Time-in :</td>
+					<td>
+						<table>
+							<tr>
+								<td><select name="hour_in">
+										<?php for ($i = 1; $i < 13; $i++):?>
+										<?php if ($i < 10) $i = "0".$i; ?>
+										<option value="<?php echo $i; ?>"
+										<?php if ($time_in['hour'] == $i) echo 'selected = "selected"'; ?>>
+											<?php echo $i; ?>
+										</option>
+										<?php endfor; ?>
+								</select></td>
+								<td>:</td>
+								<td><select name="minute_in">
+										<?php for ($i = 0; $i < 60; $i+=15):?>
+										<?php if ($i == 0) $i = "00"; ?>
+										<option value="<?php echo $i; ?>"
+										<?php if ($time_in['minute'] == $i) echo 'selected="selected"'; ?>>
+											<?php echo $i; ?>
+										</option>
+										<?php endfor; ?>
+								</select></td>
+								<td><select name="pm_in"><option value="am"
+								<?php if ($time_in['period'] == "am") echo 'selected="selected"'; ?>>AM</option>
+										<option value="pm"
+										<?php if ($time_in['period'] == "pm") echo 'selected="selected"'; ?>>PM</option>
+								</select>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>Time-out :</td>
+					<td>
+						<table>
+							<tr>
+								<td><select name="hour_out">
+										<?php for ($i = 1; $i < 13; $i++):?>
+										<?php if ($i < 10) $i = "0".$i; ?>
+										<option value="<?php echo $i; ?>"
+										<?php if ($time_out['hour'] == $i) echo 'selected = "selected"'; ?>>
+											<?php echo $i; ?>
+										</option>
+										<?php endfor; ?>
+								</select></td>
+								<td>:</td>
+								<td><select name="minute_out">
+										<?php for ($i = 0; $i < 60; $i+=15): ?>
+										<?php if ($i == 0) $i = "00"; ?>
+										<option value="<?php echo $i; ?>"
+										<?php if ($time_out['minute'] == $i) echo 'selected = "selected"'; ?>>
+											<?php echo $i; ?>
+										</option>
+										<?php endfor; ?>
+								</select></td>
+								<td><select name="pm_out"><option value="am"
+								<?php if ($time_out['period'] == "am") echo 'selected = "selected"'; ?>>AM</option>
+										<option value="pm"
+										<?php if ($time_out['period'] == 'pm') echo 'selected = "selected"'; ?>>PM</option>
+								</select>
+								</td>
+							</tr>
+						</table>
 					</td>
 				</tr>
 			</table>
 		</td>
-	</tr>
-	<tr>
-		<td><input type="submit" value="Log Time" />
-		</td>
-	</tr>
-</table>
-</form>
+
+		<table>
+			<tr>
+				<td><input type="submit" value="Log Time" />
+				</td>
+			</tr>
+		</table>
+		</form>
