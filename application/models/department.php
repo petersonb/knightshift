4de51a4a6ad43e52b2b5eae7395c5e999025a20f
Brett Peterson <bepeterson@petersonb.com>
2013-09-30 14:28:11 -0500

    added shifts to the department and employee models. (whoops forgot about these)

--- application/models/department.php
+++ application/models/department.php
@@ -2,57 +2,58 @@
 
 class Department extends DataMapper {
 	
-	var $validation = array (
-			array(
+  var $validation = array (
+			   array(
 				 'field' => 'password',
 				 'label' => 'Password',
 				 'rules' => array('encrypt'),
 				 'type'  => 'password'
-			)
-	);
+				 )
+			   );
 
-	var $has_many = array(
+  var $has_many = array(
 			'admin' => array('join_table' => 'admins_departments'),
 			'employee' => array('join_table' => 'employees_departments'),
 			'rate'=>array('join_table'=>'employees_departments'),
 			'hour' => array('join_table' => 'hour_relations'),
-			'notification'=>array('join_table'=>'hour_relations')
-	);
+			'notification'=>array('join_table'=>'hour_relations'),
+			'shift'=>array('join_table'=>'shift_relations'),
+			);
 
-	function __construct($id = NULL)
-	{
-		parent::__construct($id);
-	}
+  function __construct($id = NULL)
+  {
+    parent::__construct($id);
+  }
 
-	function login()
-	{
-		$name = $this->login_name;
+  function login()
+  {
+    $name = $this->login_name;
 
-		$d = new Department();
+    $d = new Department();
 
-		$d->where('login_name',$name)->get();
-		$pass = $d->password;
-		$passFrag = explode(':',$pass);
-		$this->salt = $passFrag[0];
-		$this->validate()->get();
+    $d->where('login_name',$name)->get();
+    $pass = $d->password;
+    $passFrag = explode(':',$pass);
+    $this->salt = $passFrag[0];
+    $this->validate()->get();
 
-		if ($this->exists())
-			return TRUE;
-		else
-			return FALSE;
-	}
+    if ($this->exists())
+      return TRUE;
+    else
+      return FALSE;
+  }
 
-	function _encrypt($field)
-	{
-		if (!empty($this->{$field}))
-		{
-			if (empty($this->salt))
+  function _encrypt($field)
+  {
+    if (!empty($this->{$field}))
+      {
+	if (empty($this->salt))
 	  {
-	  	$this->salt = uniqid();
+	    $this->salt = uniqid();
 	  }
-	  $this->{$field} = $this->salt . ':' . hash('sha256', $this->salt . $this->{$field});
-		}
-	}
+	$this->{$field} = $this->salt . ':' . hash('sha256', $this->salt . $this->{$field});
+      }
+  }
 }
 
 /* End of file department.php */
--- application/models/employee.php
+++ application/models/employee.php
@@ -2,58 +2,59 @@
 
 class Employee extends DataMapper {
 
-	var $validation = array (
-			array(
+  var $validation = array (
+			   array(
 				 'field' => 'password',
 				 'label' => 'Password',
 				 'rules' => array('encrypt'),
 				 'type'  => 'password'
-			)
-	);
+				 )
+			   );
 
 
-	var $has_one = array('password_change_request');
-	var $has_many = array(
+  var $has_one = array('password_change_request');
+  var $has_many = array(
 			'department' => array('join_table'=>'employees_departments'),
 			'rate'=>array('join_table'=>'employees_departments'),
 			'hour' => array('join_table'=>'hour_relations'),
 			'notification' => array('join_table'=>'notification_relations')
-	);
+			'shift'=>array('join_table'=>'shift_relations'),
+			);
 
-	function __construct($id = NULL)
-	{
-		parent::__construct($id);
-	}
+  function __construct($id = NULL)
+  {
+    parent::__construct($id);
+  }
 
-	function login()
-	{
-		$email = $this->email;
+  function login()
+  {
+    $email = $this->email;
 
-		$e = new Employee();
+    $e = new Employee();
 
-		$e->where('email',$email)->get();
-		$pass = $e->password;
-		$passFrag = explode(':',$pass);
-		$this->salt = $passFrag[0];
-		$this->validate()->get();
+    $e->where('email',$email)->get();
+    $pass = $e->password;
+    $passFrag = explode(':',$pass);
+    $this->salt = $passFrag[0];
+    $this->validate()->get();
 
-		if ($this->exists())
-			return TRUE;
-		else
-			return FALSE;
-	}
+    if ($this->exists())
+      return TRUE;
+    else
+      return FALSE;
+  }
 
-	function _encrypt($field)
-	{
-		if (!empty($this->{$field}))
-		{
-			if (empty($this->salt))
+  function _encrypt($field)
+  {
+    if (!empty($this->{$field}))
+      {
+	if (empty($this->salt))
 	  {
-	  	$this->salt = uniqid();
+	    $this->salt = uniqid();
 	  }
-	  $this->{$field} = $this->salt . ':' . hash('sha256', $this->salt . $this->{$field});
-		}
-	}
+	$this->{$field} = $this->salt . ':' . hash('sha256', $this->salt . $this->{$field});
+      }
+  }
 }
 
 /* End of file employee.php */
